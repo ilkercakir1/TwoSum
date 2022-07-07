5c959e070aa370271a2ff0a19544fa9ecabc890c
İlker Çakır <ilker@DESKTOP-2KQN64B>
2022-07-07 10:37:31 +0300

    Proje dosyası ekle.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+
+public class Program
+{
+
+    static public void Main()
+    {
+
+        Console.ForegroundColor = ConsoleColor.Green;
+
+
+        int[] sayilar = new int[] { 11, 3, 5, 7, 9 };
+        int target = 18;
+
+        for (int i = 0; i < sayilar.Length; i++)
+        {
+
+            for (int j = i + 1; j < sayilar.Length; j++)
+            {
+                if (sayilar[i] + sayilar[j] == target)
+                    
+                    Console.WriteLine("Sayılar -> {0}. index({1}) ve {2}. index({3})", i+1, sayilar[i], j+1, sayilar[j]);
+
+            }
+
+        }
+
+        Console.ReadKey();
+    }
+
+}
